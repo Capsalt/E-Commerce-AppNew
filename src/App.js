3d7f2d348ee71ee8fa6a18017c4fcba57940c751
Capsalt <hbasri2304@gmail.com>
2022-02-26 21:36:14 +0300

    v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopBar from './components/common/TopBar'
+import ProductCard from './components/products/ProductCard'
+
+const App = () => {
+  return (
+    <>
+      <TopBar/>
+      <ProductCard/>
+    </>
+  )
+}
+
+export default App
